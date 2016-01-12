1b4f6520cb0fa2ffc355b078080d4c39efe54b7b
Striking <Strikingwolf2012@gmail.com>
2016-01-12 16:44:54 -0600

    Spinner controls

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/wave/WaveManual.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/wave/WaveManual.java
@@ -5,5 +5,22 @@ package com.qualcomm.ftcrobotcontroller.opmodes.wave;
  * Manual period code
  */
 public class WaveManual extends WaveTele {
-    // TODO
+    protected boolean spinToggle = false;
+
+    @Override
+    public void loop() {
+        joystickMotors(gamepad2.left_stick_y, liftMotor1);
+        joystickMotors(gamepad2.right_stick_y, liftMotor2);
+
+        if (gamepad1.right_bumper) {
+            setMotors(1, spinnerMotor);
+        } else if (gamepad1.left_bumper) {
+            setMotors(-1, spinnerMotor);
+        } else {
+            setMotors(0, spinnerMotor);
+        }
+
+        super.loop();
+    }
+
 }
