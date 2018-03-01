64b6c8a838e5b23e2d9b55caba167a34ae7d45b2
TomRaku <31420335+TomRaku@users.noreply.github.com>
2018-03-01 15:51:43 -0600

    Blank Slate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBot.java
@@ -12,22 +12,10 @@ import java.util.concurrent.TimeUnit;
 
 // Declare that the OpMode is Autonomous and is named AutoBot
 @Autonomous(name="AutoBot", group="Auto")
-public class AutoBot extends Auto {
+public class AutoBot extends OpMode {
 
     /* Declare OpMode members. */
 
     HardwareBot robot;
-
-    @Override
-    public void init(){
-        robot = new HardwareBot();   // Use our hardware
-        robot.mecanum(hardwareMap);
-    }
-
-    @Override
-    public void main() {
-        DcMotor[] motors = new DcMotor[]{robot.fl, robot.br, robot.fr, robot.bl};
-        AutoUtil.beforeAfter(AutoUtil.setMotorsCall(0.5, motors), AutoUtil.setMotorsCall(0, motors), robot.period, 1000);
-    }
 }
 
